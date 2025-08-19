d7e082c348f2703395b5b80267b74e0de5b5cab0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-19 09:50:57 -0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const port = 5001;
+
+app.get("/test", (req, res)=>{
+    res.send("Olá Servidor teste Ok!")
+})
+
+app.get("/system", (req, res)=>{
+    res.send("Seja Bem-Vindo ao Sistema!")
+})
+
+app.get("/about", (req, res)=>{
+    res.send("Página Sobre!")
+})
+
+app.listen(port, ()=>{
+    console.log(`Sevidor rodando http://localhost:${port}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  
+  //Declaração de Variáveis
+  let nome = "Fiap";
+  let apelido = "Fiapinho";
+  let novoNome = nome.toUpperCase();
+
+  //Criando Função
+  
+  function soma(valor1,valor2){
+    return valor1 + valor2
+  }
+
+  //Arrow Function
+  const desconto = (valor3,valor4)=>{
+    return valor3 - valor4
+  }
+  function enviar(){
+    alert(`Seja Bem-Vindo ${apelido}`)
+  }
+
+  return (
+    <>
+      <h1>Seja Bem-Vindo {novoNome} {apelido}</h1>
+      <h2>A soma da Função é: {soma(10,8)}</h2>
+      <h3>o desconto da Arrow Function é: {desconto(10,8)}%</h3>
+      <button onClick={enviar}>Clique Aqui</button>
+    </>
+  )
+}
+
+export default App
